9d9a516c45f032127aa79ae676a76ef1f5aeac54
Glauco Beserra <dantas.glauco@gmail.com>
2023-10-13 02:14:41 -0300

    Section 4 Create a custom image view

---
+++ Landmarks/Landmarks/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Glauco Dantas Beserra on 13/10/23.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay{
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
