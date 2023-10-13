ac8dfaddd7ac6c48ce4f3b5b2046e6b0d238e683
Glauco Beserra <dantas.glauco@gmail.com>
2023-10-13 01:31:02 -0300

    Section 1 - Create a new project and explore the canvas

---
+++ Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Glauco Dantas Beserra on 13/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
