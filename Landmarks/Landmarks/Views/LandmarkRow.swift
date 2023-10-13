3f5c5e63b402ff3a9589806bdad854aa912a7dd1
Glauco Beserra <dantas.glauco@gmail.com>
2023-10-13 05:09:57 -0300

    Section 2 Create the row view

---
+++ Landmarks/Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,27 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Glauco Dantas Beserra on 13/10/23.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    LandmarkRow(landmark: landmarks[0])
+}
