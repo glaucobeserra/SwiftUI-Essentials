45b07e16a8346254b31c8417da9ac6d9109fae58
Glauco Beserra <dantas.glauco@gmail.com>
2023-10-13 22:51:44 -0300

    Section 6 Create a favorite button for each landmark

---
+++ Landmarks/Landmarks/Views/Helper/FavoriteButton.swift
@@ -0,0 +1,27 @@
+//
+//  FavoriteButton.swift
+//  Landmarks
+//
+//  Created by Glauco Dantas Beserra on 13/10/23.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    
+    
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundStyle(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+#Preview {
+    FavoriteButton(isSet: .constant(true))
+}
